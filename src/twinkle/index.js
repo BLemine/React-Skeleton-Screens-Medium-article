8a2c56866b894c993b8e740815d63eca9135ab9f
BLemine <baillahiamine@gmail.com>
2020-11-29 17:43:05 +0100

    added-css-animation

---
+++ src/twinkle/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.css";
+
+const Twinkle = () => {
+  return (
+    <div className="twinkle-wrapper">
+      <div className="twinkle"></div>
+    </div>
+  )
+}
+
+export default Twinkle;
