4501e371b63bd5655292d4f122497db3f1f5fb92
BLemine <baillahiamine@gmail.com>
2020-11-29 03:47:37 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState, Fragment } from "react";
+import Skeleton from './skeleton'
 function App() {
+  const [poetry, setPoetry] = useState([]);
+
+  const fetchPoetry = async () => {
+    fetch("https://www.poemist.com/api/v1/randompoems")
+      .then(res => res.json())
+      .then(resp => setPoetry(resp))
+  }
+  useEffect(() => {
+    fetchPoetry();
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" >
+      <div className="poetryContainer">
+        <h2 style={{ textAlign: "center" }}>Quick Random Poetry</h2>
+        {poetry.length !== 0 ? poetry.map((post, index) => {
+          return <div key={index}>
+            <p><span style={{ fontWeight: "bold" }}>Title : </span>{post.title}</p>
+            <p><span style={{ fontWeight: "bold" }}>Poet : </span>{post.poet.name}</p>
+            <p><span style={{ fontWeight: "bold" }}>Text : </span>{post.content}</p>
+          </div>
+        })
+          :
+          <>
+            {[1, 2, 3, 4].map(index => {
+              return (
+                <Fragment key={index}>
+                  <Skeleton type="title" />
+                  <Skeleton type="title" />
+                  <Skeleton type="text" />
+                </Fragment>
+              )
+            })}
+          </>
+        }
+      </div>
     </div>
   );
 }
---
+++ src/skeleton/index.js
@@ -0,0 +1,8 @@
+import "./style.css";
+
+export default function Skeleton({type}){
+    const classes=`skeleton-${type}`;
+    return (
+        <div className={classes}></div>
+    )
+}
