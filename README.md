e90ffb2644fe0a46fb9dbdf12d7900c7536e1051
BLemine <baillahiamine@gmail.com>
2020-11-29 22:38:11 +0100

    avatar

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-### About : 
-It's about an article that i wrote on **Medium** about React Skeleton Screen
+### About :grey_exclamation: : 
+It's about an article :black_nib: that i wrote on **Medium** about **React Skeleton Screen.**
+I took as an example a sample app that shows a random poems for a random poet :scroll: .
+You can check out my Poetry database provider from [here](https://poetrydb.org/index.html) 
+And you can find the article my article [here](https://baillahiamine.medium.com/)
--- src/App.js
+++ src/App.js
@@ -3,38 +3,60 @@ import { useEffect, useState, Fragment } from "react";
 import Skeleton from './skeleton'
 function App() {
   const [poetry, setPoetry] = useState([]);
+  const [poet, setPoet] = useState("");
 
-  const fetchPoetry = async () => {
-    fetch("https://www.poemist.com/api/v1/randompoems")
+  const fetchPoetry = () => {
+    fetch("https://poetrydb.org/author")
       .then(res => res.json())
-      .then(resp => setPoetry(resp))
+      .then(resp => {
+        const chosenPoet = resp.authors[Math.floor(resp.authors.length * Math.random())]
+        setPoet(chosenPoet)
+        fetch("https://poetrydb.org/author/" + chosenPoet)
+          .then(res => res.json())
+          .then(resp => setPoetry(resp))
+      }
+      )
   }
+
   useEffect(() => {
-    setTimeout(()=>fetchPoetry(),5000);
+    setTimeout(() => fetchPoetry(), 5000);
     //fetchPoetry();
   }, [])
   return (
-    <div className="container" >
+    <div>
       <div className="poetryContainer">
         <h2 style={{ textAlign: "center" }}>Quick Random Poetry</h2>
-        {poetry.length !== 0 ? poetry.map((post, index) => {
-          return <div key={index}>
-            <p><span style={{ fontWeight: "bold" }}>Title : </span>{post.title}</p>
-            <p><span style={{ fontWeight: "bold" }}>Poet : </span>{post.poet.name}</p>
-            <p><span style={{ fontWeight: "bold" }}>Text : </span>{post.content}</p>
-          </div>
-        })
+        {poetry.length !== 0 ?
+          <>
+            {poetry.map((poem, index) => {
+              if (index <= 10) {
+                return <div key={index} style={{ marginBottom: 30 }}>
+                  <span style={{ display: "block", marginBottom: 10 }}><span style={{ fontWeight: "bold" }}>Title : </span>{poem.title}</span>
+                  <span style={{ display: "block" }}><span style={{ fontWeight: "bold" }}>Text : </span>{poem.lines.toString().substring(0, 500)}</span>
+                </div>
+              } else return null;
+            })}
+            <div style={{ display: "inline-block" }}>
+              <div style={{ textAlign: "center", position: "absolute", top: 20, right: 20 }}>
+                <img alt="best_author" style={{ borderRadius: 50, }} src="https://www.lexiconn.in/wp-content/uploads/2019/04/FI-Genres-of-peotry..jpg" width={150} height={150} />
+                <h4>{poet}</h4>
+              </div>
+            </div>
+          </>
           :
           <>
             {[1, 2, 3, 4].map(index => {
               return (
                 <Fragment key={index}>
                   <Skeleton type="title" />
-                  <Skeleton type="title" />
                   <Skeleton type="text" />
                 </Fragment>
               )
             })}
+            <div className="poetSkeletonContainer">
+              <Skeleton type="avatar" />
+              <Skeleton type="title" />
+            </div>
           </>
         }
       </div>
