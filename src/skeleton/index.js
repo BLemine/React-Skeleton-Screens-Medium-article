cd210532da85574afc4c88570daaf4ca71b8129b
BLemine <baillahiamine@gmail.com>
2020-11-29 18:15:42 +0100

    refactor-animation

--- src/skeleton/index.js
+++ src/skeleton/index.js
@@ -3,9 +3,11 @@ import Twinkle from '../twinkle'
 export default function Skeleton({ type }) {
     const classes = `skeleton-${type}`;
     return (
-        <>
-            <div className={classes}></div>
-            <Twinkle />
-        </>
+        <div className="skeleton-wrapper">
+            <div className={classes}>
+                <Twinkle />
+            </div>
+            
+        </div>
     )
 }
